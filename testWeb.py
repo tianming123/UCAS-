9e39cdeb4bb98f552fc5d59a4768e3a42a304698
tianming123 <40027762+tianming123@users.noreply.github.com>
2020-11-13 13:12:52 +0000

    Update testWeb.py

--- testWeb.py
+++ testWeb.py
@@ -4,8 +4,8 @@ driver = webdriver.Chrome()
 url = "https://onestop.ucas.edu.cn/home/index"
 driver.get(url)
 
-username = driver.find_element_by_id("menhuusername").send_keys("1051250432@qq.com")
-pwd = driver.find_element_by_id("menhupassword").send_keys("tianming123")
+username = driver.find_element_by_id("menhuusername").send_keys("你的账户")
+pwd = driver.find_element_by_id("menhupassword").send_keys("你的密码")
 click = driver.find_element_by_class_name("loginbtn").click()
 time.sleep(3)
 click2 = driver.find_element_by_xpath('//*[@id="main-metro"]/div/div[1]/a').click()
